fc653ae395149c17f0af4d95876c845a6d4136a4
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-28 16:05:19 +0530

    Buged ->> Fixed

---
+++ Configs.py
@@ -0,0 +1,12 @@
+from decouple import config
+
+class Var:
+    API_ID = config("API_ID", default=6)
+    API_HASH = config("API_HASH", None)
+    BOT_TOKEN = config("BOT_TOKEN", None)
+    CONSUMER_KEY = config("CONSUMER_KEY", None)
+    CONSUMER_SECRET = config("CONSUMER_SECRET", None)
+    ACCESS_TOKEN = config("ACCESS_TOKEN", None)
+    ACCESS_TOKEN_SECRET = config("ACCESS_TOKEN_SECRET", None)
+    TO_CHAT = config("TO_CHAT", None)
+    TRACK_USERS = config("TRACK_USERS", None)
