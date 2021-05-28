61472f765dee97ce0b0f84505bf4b151f93635bb
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-28 15:44:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
+# Telegram - Twitter - Bot
+# Github.com/New-dev0/TgTwitterStreamer
+# CopyLeft AGPLv3 (C) 2020 The Authors
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import tweepy
 from Configs import Var
-
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 from telethon.tl.custom import Button
 
 from tweepy.asynchronous import AsyncStream
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
 
-Client = TelegramClient("TG-Twitter Streamer", Var.API_ID,
-                Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
+Client = TelegramClient("TG-Twitter Streamer", Var.API_ID, Var.API_HASH).start(
+    bot_token=Var.BOT_TOKEN
+)
 
 auth = tweepy.OAuthHandler(Var.CONSUMER_KEY, Var.CONSUMER_SECRET)
 auth.set_access_token(Var.ACCESS_TOKEN, Var.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-
-print("Setting Up Bot !")
+print("<<--- Setting Up Bot ! --->>")
 TRACK_IDS = []
 for userid in Var.TRACK_USERS.split(" "):
     try:
         user = api.get_user(screen_name=userid)._json
         TRACK_IDS.append(str(user["id"]))
-        print(f"Added {user['screen_name']} to TRACK - LIST !")
+        print(f"<<--- Added {user['screen_name']} to TRACK - LIST ! --->>")
     except Exception as e:
         print(e)
 
@@ -31,6 +38,7 @@ for userid in Var.TRACK_USERS.split(" "):
 class TgStreamer(AsyncStream):
     async def on_connect(self):
         print("<<<---||| Stream Connected |||--->>>")
+
     async def on_status(self, status):
         tweet = status._json
         user = tweet["user"]
@@ -46,17 +54,40 @@ class TgStreamer(AsyncStream):
             Var.TO_CHAT,
             text,
             link_preview=False,
-            buttons=Button.url(text="View 🔗", url=url))
-    
+            buttons=Button.url(text="View 🔗", url=url),
+        )
+
     async def on_connection_error(self):
-        print("Connection Error, Disconnecting...")
-    
+        print("<<---|| Connection Error ||--->>")
+
     async def on_exception(self, exception):
         print(exception)
 
 
+@Client.on(events.NewMessage(pattern=r"/start"))
+async def startmsg(event):
+    await event.reply(
+        "Hi, I am Alive !",
+        buttons=[
+            [
+                Button.url(
+                    "TgTwitterStreamer",
+                    url="https://github.com/New-dev0/TgTwitterStreamer",
+                )
+            ],
+            [Button.url("Support Group", url="t.me/FutureCodesChat")],
+        ],
+    )
+
+
 if __name__ == "__main__":
-    Stream = TgStreamer(Var.CONSUMER_KEY,Var.CONSUMER_SECRET,Var.ACCESS_TOKEN,Var.ACCESS_TOKEN_SECRET)
+    Stream = TgStreamer(
+        Var.CONSUMER_KEY,
+        Var.CONSUMER_SECRET,
+        Var.ACCESS_TOKEN,
+        Var.ACCESS_TOKEN_SECRET
+    )
     Stream.filter(follow=TRACK_IDS)
+
     with Client:
         Client.run_until_disconnected()  # Running Client
